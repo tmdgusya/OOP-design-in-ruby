f2485a67fe6855478b8e47830107871f21279813
roach <tmdgusya@naver.com>
2021-09-16 22:15:05 +0900

    너무 많은 일을 하고 있기에 쓸모없는 속성제거

--- Bicycle/gear.rb
+++ Bicycle/gear.rb
@@ -1,18 +1,16 @@
 class Gear
-    attr_reader :chainring, :cog, :rim, :tire
-    def initialize(chainring, cog, rim, tire)
+    attr_reader :chainring, :cog
+    def initialize(chainring, cog)
       @chainring = chainring
       @cog = cog
-      @rim = rim
-      @tire = tire
     end
 
     def ratio
         chainring / cog.to_f
     end
 
-    def gear_inches
-        ratio * (rim + (tire * 2))
+    def cog
+        @cog
     end
     
 end
